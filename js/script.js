7d9cc8baff323dd8dee59dedb2b2d9e5642d9a4a
shakhawat <shakhawathossen188@gmail.com>
2023-09-10 16:53:07 +0600

    footer desigh

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
 
-  AOS.init();
+  // let vartical_manu = document.querySelector(".vartical_menu_title");
+  // vartical_manu.classList.toggle("active");
+  $(document).ready(function(){
+    $(".vartical_menu_title").click(function(){
+      $(".vertical_menu").toggleClass("active");
+    });
+  }); 
+ AOS.init(); 
